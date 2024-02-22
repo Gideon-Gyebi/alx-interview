1eb053f198025c927eb59ab9244e4b8ceaa280a4
Gideon-Gyebi <125477741+Gideon-Gyebi@users.noreply.github.com>
2024-02-22 00:01:23 +0000

    Documentations 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-'''2D matrix'''
+'''Rotate 2D Matrix'''
 
 
 def rotate_2d_matrix(matrix):
-    '''rotates a 2d matrix 90° clockwise
+    '''rotating a 2d matrix 90° clockwise
     Returns: Nothing'''
     left, right = 0, len(matrix) - 1
 
     while left < right:
         for i in range(right - left):
             top, bottom = left, right
-            # save topleft  value
+            # saves topleft value
             topLeft = matrix[top][left + i]
-            # move bottom left to top left
+            # moves bottom left to top left
             matrix[top][left + i] = matrix[bottom - i][left]
-            # move bottom right to bottom left
+            # moves bottom right to bottom left
             matrix[bottom - i][left] = matrix[bottom][right - i]
-            # move top right to bottom right
+            # moves top right to bottom right
             matrix[bottom][right - i] = matrix[top + i][right]
-            # move top left to top right
+            # moves top left to top right
             matrix[top + i][right] = topLeft
         right -= 1
         left += 1
